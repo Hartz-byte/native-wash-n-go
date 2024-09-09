ed03f066a4e7d622bd1e5cf36745190bf417af83
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-09-10 00:29:30 +0530

    welcome screen done

--- app/(auth)/Welcome.tsx
+++ app/(auth)/Welcome.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import Logo from "../../assets/images/logo.png";
+import CustomButton from "@/components/CustomButton";
 
 const Welcome = () => {
+  const logoutHandler = () => {};
+
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <SafeAreaView style={styles.mainContainer}>
+      {/* logo */}
+      <View style={styles.logoContainer}>
+        <Image source={Logo} style={styles.logo} />
+      </View>
+
+      {/* welcome text */}
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Welcome Ramesh</Text>
+      </View>
+
+      {/* custom button */}
+      <CustomButton text="Logout" press={logoutHandler} />
+    </SafeAreaView>
   );
 };
 
-export default Welcome;
+const styles = StyleSheet.create({
+  mainContainer: {
+    padding: 10,
+  },
+  logoContainer: {
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  logo: {
+    width: 233,
+    height: 180,
+  },
+  textContainer: {
+    alignItems: "center",
+    marginTop: 60,
+    marginBottom: 60,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 32,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default Welcome;
